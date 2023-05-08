47ae98eb7957c11185f878fd6580fbc3be37b80a
Ivan Samyshev <102234815+Mesonti@users.noreply.github.com>
2023-05-08 21:23:11 +0400

    fix headers

--- task-string.c
+++ task-string.c
@@ -1,20 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <conio.h>
 #include <locale.h>
-#include <windows.h>
-#include <winuser.h>
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
     char a[10] = "абв";
-    
 
     gets(a);
     OemToChar(a, a);
     printf("After translation %s\n", a);
-    
+
     return 0;
 }
